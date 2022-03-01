f20bf7fce58944e0a262f5939caf34acc2d07322
Jakobi Haskell <jakobi_haskell@brown.edeu>
2022-03-01 03:38:09 -0500

    order

--- script.js
+++ script.js
@@ -80,8 +80,8 @@ window.setInterval(() => {
   const tweetball = document.createElement('a-sphere');
   tweetball.setAttribute("radius", `0.1`);
   tweetball.setAttribute("color", `#000000`);
+  const numFs = fakeData.filter(x => x === 'f').length;
   const color = Math.round(255 * numFs / 15);
-    const numFs = fakeData.filter(x => x === 'f').length;
   tweetball.setAttribute("color", `rgb(${color}, 0, 0)`);
   tweetball.setAttribute("position", `0 ${fakeData[currIndex].length / 20} -3`)
   console.log("tweetball received: ");
